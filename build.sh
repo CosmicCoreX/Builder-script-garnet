8c8852d1286558e6e1528b27b833747de23eb585
Z E U S <iamdrzeus14@gmail.com>
2026-07-19 23:03:56 -0700

    Update repository URL for Builder-script clone

--- build.sh
+++ build.sh
@@ -54,7 +54,7 @@ function clean() {
 
 function sync_sources() {
   repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs --depth=1
-  git clone https://github.com/iamzeus14/Builder-script -b main .repo/local_manifests
+  git clone https://github.com/CosmicCoreX/Builder-script.git -b main .repo/local_manifests
 
   if [ -f /opt/crave/resync.sh ]; then
     /opt/crave/resync.sh
@@ -76,7 +76,7 @@ function setup_env() {
 
 function build_rom() {
   touch "$LOG"
-  ax -b 2>&1 | tee "$LOG" &
+  m bacon 2>&1 | tee "$LOG" &
   BUILD_PID=$!
 
   wait "$BUILD_PID"
